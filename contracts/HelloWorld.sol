58e0fa8442ac167968e5444a31c6b0b4ab758ef0
akashramj <akash@taxnodes.com>
2023-06-14 20:02:54 +0530

    Added Files

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,13 +4,13 @@ pragma solidity >=0.8.9;
 
 contract HelloWorld {
    event UpdatedMessages(string oldStr, string newStr);
-   string public message;
-   constructor(string memory initMessage) {
-      message = initMessage;
+   string public messageVal;
+   constructor(string memory initialMessage) {
+      messageVal = initialMessage;
    }
    function update(string memory newMessage) public {
-      string memory oldMsg = message;
-      message = newMessage;
-      emit UpdatedMessages(oldMsg, newMessage);
+      string memory oldMessage = messageVal;
+      messageVal = newMessage;
+      emit UpdatedMessages(oldMessage, newMessage);
    }
 }
